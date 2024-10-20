8328bc75bd44e88e248c0c933422581b428e331b
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-20 23:19:36 +0700

    update

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(express.json())
 app.use(cors({
-    origin: ['https://trello-app-1.vercel.app', 'http://localhost:3000'],
+    origin: ['https://trello-app-1.vercel.app', 'http://localhost:3000', 'https://trello-api-test.onrender.com'],
     credentials: true,
 }))
 
