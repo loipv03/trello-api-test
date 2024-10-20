4bcecb3a958652d30c57b76afb2402ee4d0c72be
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-20 21:33:21 +0700

    update

--- src/controller/auth/login.ts
+++ src/controller/auth/login.ts
@@ -36,19 +36,19 @@ const login = async (req: AuthenticatedRequest, res: Response, next: NextFunctio
 
         res.cookie('access_token', access_token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'none',
             path: '/',
-            // domain: 'https://trello-app-1.vercel.app',
+            domain: 'https://trello-app-1.vercel.app',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
         res.cookie('refresh_token', refresh_Token, {
             httpOnly: true,
-            secure: false,
+            secure: true,
             sameSite: 'none',
             path: '/',
-            // domain: 'https://trello-app-1.vercel.app',
+            domain: 'https://trello-app-1.vercel.app',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
