c6096be68f47c4d8a7a531a1f1c26ab600bbd70c
phanvanloi <loipvph24552@fpt.edu.vn.com>
2024-10-20 22:38:54 +0700

    update

--- src/controller/auth/login.ts
+++ src/controller/auth/login.ts
@@ -39,7 +39,7 @@ const login = async (req: AuthenticatedRequest, res: Response, next: NextFunctio
             secure: true,
             sameSite: 'strict',
             path: '/',
-            // domain: 'https://trello-app-1.vercel.app',
+            domain: 'trello-app-1.vercel.app',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
